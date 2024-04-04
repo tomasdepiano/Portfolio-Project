58885a65fbc3f9fadd59846ff1e56e75627566ee
Tomas De Piano <tomasdepiano1@gmail.com>
2024-04-04 11:23:10 -0600

    Integrated emailjs to this app; Got the form on the contact to work properly and it sent me a test email to my actual gmail account

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,7 @@
+import { useRef, useState } from "react";
 import "./contact.scss";
 import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
 
 const variants = {
   initial: {
@@ -17,6 +19,31 @@ const variants = {
 };
 
 const Contact = () => {
+  const formRef = useRef();
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_9tnmi7m",
+        "template_1g3xply",
+        formRef.current,
+        "DxuH8H79NQo4r6YIf"
+      )
+      .then(
+        (result) => {
+          setSuccess(true);
+          console.log(result.text);
+        },
+        (error) => {
+          setError(true);
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <motion.div
       className="contact"
@@ -36,11 +63,13 @@ const Contact = () => {
         </motion.div>
       </motion.div>
       <div className="formContainer">
-        <form>
-          <input type="text" required placeholder="Name" />
-          <input type="text" required placeholder="Email" />
-          <textarea rows={8} placeholder="Message" />
+        <form ref={formRef} onSubmit={sendEmail}>
+          <input type="text" required placeholder="Name" name="name" />
+          <input type="text" required placeholder="Email" name="email" />
+          <textarea rows={8} placeholder="Message" name="message" />
           <button>Submit</button>
+          {error && "Error"}
+          {success && "Success"}
         </form>
       </div>
     </motion.div>
