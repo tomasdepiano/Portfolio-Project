762ad3c9f533449ac30624a61cc3d250a65dbe52
Tomas De Piano <tomasdepiano1@gmail.com>
2024-04-04 20:51:42 -0600

    Updated the app.jsx, had a typo on the hero to be capatilized on the import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./app.scss";
 import Navbar from "./components/navbar/Navbar";
-import Hero from "./components/hero/hero";
+import Hero from "./components/hero/Hero";
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
 import Contact from "./components/contact/Contact";
